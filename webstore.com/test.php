8fcede9915ac97d19877277bc1064d9edec0af08
Max Koval <kova1max99@gmail.com>
2019-07-06 17:54:44 +0300

    asd

--- webstore.com/test.php
+++ webstore.com/test.php
@@ -1,14 +1,13 @@
 <?php 
     require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
     $APPLICATION->SetTitle("TEST PAGE");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.feedback",
-	"feedback",
-	Array(
-		"EMAIL_TO" => "sale@webstore.com",
-		"EVENT_MESSAGE_ID" => array(),
-		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
-		"REQUIRED_FIELDS" => array(),
-		"USE_CAPTCHA" => "Y"
-	)
-);?><br>
+?>
+<?
+echo '<pre>';
+print_r('TEST PAGE');
+echo '</pre>';
+?>
+
+<?php 
+    require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+?>
