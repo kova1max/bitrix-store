03e5ae6494e333998787f7eb2886c92617569516
Max Koval <kova1max99@gmail.com>
2019-07-16 14:49:58 +0300

    wednesday change

--- webstore.com/test.php
+++ webstore.com/test.php
@@ -1,47 +1,6 @@
 <?php 
     require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
     $APPLICATION->SetTitle("TEST PAGE");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:search.page",
-	"custom-search",
-	Array(
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_SHADOW" => "Y",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "N",
-		"DEFAULT_SORT" => "rank",
-		"DISPLAY_BOTTOM_PAGER" => "N",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FILTER_NAME" => "",
-		"NO_WORD_LOGIC" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => "arrows",
-		"PAGER_TITLE" => "Результаты поиска",
-		"PAGE_RESULT_COUNT" => "25",
-		"PATH_TO_USER_PROFILE" => "",
-		"RATING_TYPE" => "",
-		"RESTART" => "N",
-		"SHOW_ITEM_DATE_CHANGE" => "N",
-		"SHOW_ITEM_TAGS" => "N",
-		"SHOW_ORDER_BY" => "N",
-		"SHOW_RATING" => "",
-		"SHOW_TAGS_CLOUD" => "N",
-		"SHOW_WHEN" => "N",
-		"SHOW_WHERE" => "N",
-		"USE_LANGUAGE_GUESS" => "Y",
-		"USE_SUGGEST" => "N",
-		"USE_TITLE_RANK" => "N",
-		"arrFILTER" => array("iblock_catalog"),
-		"arrFILTER_iblock_catalog" => array("14"),
-		"arrFILTER_iblock_news" => array("all"),
-		"arrFILTER_iblock_services" => array("all"),
-		"arrFILTER_main" => array("")
-	)
-);?><?php 
+?><?php 
     require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
 ?>
