b39ec3acbd384301e66a984276663c37a364a40f
Max Koval <kova1max99@gmail.com>
2020-07-16 14:40:28 +0300

    delete some in test.php

--- webstore.com/test.php
+++ webstore.com/test.php
@@ -3,10 +3,6 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
     $APPLICATION->SetTitle("TEST PAGE");
 ?>
 
-<pre>HELLO WORLD</pre>
-
-<pre>OK, GIT</pre>
-
 <?php 
     require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
 ?>
